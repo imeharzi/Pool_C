bf1f376836be4f3a78041f9d82907a9604c596ca
imane meharzi <meharziimane16@gmail.com>
2025-11-28 22:27:36 +0100

    Update twosum.c

--- Day_05/twosum.c
+++ Day_05/twosum.c
@@ -4,7 +4,7 @@ void two_sum(int arr[], int size, int target) {
     for (int i = 0; i < size; i++) {
         for (int j = i + 1; j < size; j++) {
             if (arr[i] + arr[j] == target) {
-                printf("[%d,%d]\n", i + 1, j + 1);  
+                printf("[%d,%d]\n", i , j );  
                 return;
             }
         }
